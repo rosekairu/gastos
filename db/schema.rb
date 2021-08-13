19142d69764a0f8d89a4178f58d509b4258f230f
Rose Kairu <rosekairu@gmail.com>
2021-08-13 18:04:50 +0300

    signup,signin display works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_082502) do
+ActiveRecord::Schema.define(version: 2021_08_13_140134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,16 @@ ActiveRecord::Schema.define(version: 2021_08_13_082502) do
     t.index ["account_id"], name: "index_transactions_on_account_id"
   end
 
-  
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.string "fname", null: false
+    t.string "lname", null: false
+    t.integer "age", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
